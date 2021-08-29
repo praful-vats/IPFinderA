aef28103033445eabd1daaf2b398ae01986a0e05
Praful01 <78860906+Praful010101@users.noreply.github.com>
2021-08-30 02:17:00 +0530

    branch 1

--- IPApp/urls.py
+++ IPApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import index
 
 urlpatterns = [
-    path('', Index, name='index'),
+    path('', index, name='index'),
 
 ]
--- IPApp/views.py
+++ IPApp/views.py
@@ -1,8 +1,21 @@
 # from django.shortcuts import render
+# import dns.resolver
+#
+#
+# def index(request):
+#     search = request.POST.get('search')
+#
+#     ip_address = dns.resolver.Resolver.resolve(search, 'A')[0]
+#     # for i in ip_address:
+#     context = {"ip_address": ip_address.to_text()}
+#
+#     return render(request, 'index.html', context)
+
+# from django.shortcuts import render
 # import ipapi
 #
 #
-# def Index(request):
+# def index(request):
 #     search = request.POST.get('search')
 #
 #     data = ipapi.location(ip=search, output='json')
@@ -10,13 +23,17 @@
 #     context = {"data": data}
 #
 #     return render(request, 'index.html', context)
+
 from django.shortcuts import render
 import dns.resolver
 
 
-def Index(request):
+def index(request):
     search = request.POST.get('search')
-    print('search='+search)
-    ip_address = dns.resolver.resolve(search, "A")
-    context = {"ip_address": ip_address}
+    my_resolver = dns.resolver.Resolver()
+    ip_address = my_resolver.resolve(search, "A")[0]
+    # ip_address = dns.resolver.Resolver.resolve(search, 'A')
+    # for i in ip_address:
+    ip = ip_address.to_text()
+    context = {"ip_address": ip}
     return render(request, 'index.html', context)
