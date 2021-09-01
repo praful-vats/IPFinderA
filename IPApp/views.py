a9007ae781774408c8b3e7046786266876845255
Praful01 <78860906+Praful010101@users.noreply.github.com>
2021-09-01 17:02:54 +0530

    do

--- IPApp/views.py
+++ IPApp/views.py
@@ -24,16 +24,49 @@
 #
 #     return render(request, 'index.html', context)
 
+# from django.shortcuts import render
+# import dns.resolver
+#
+#
+# def index(request):
+#     search = request.POST.get('search')
+#     my_resolver = dns.resolver.Resolver()
+#     ip_address = my_resolver.resolve(search, "A")[0]
+#     # ip_address = dns.resolver.Resolver.resolve(search, 'A')
+#     # for i in ip_address:
+#     ip = ip_address.to_text()
+#     context = {"ip_address": ip}
+#     return render(request, 'index.html', context)
+
 from django.shortcuts import render
+import dns
 import dns.resolver
 
 
 def index(request):
     search = request.POST.get('search')
-    my_resolver = dns.resolver.Resolver()
-    ip_address = my_resolver.resolve(search, "A")[0]
-    # ip_address = dns.resolver.Resolver.resolve(search, 'A')
-    # for i in ip_address:
-    ip = ip_address.to_text()
-    context = {"ip_address": ip}
+    print(search)
+    # ip_address = dns.resolver.Resolver.resolve(search, "A")
+    # ip_address = dns.resolver.query(search, "A")
+   #untag
+    ip_address = dns.resolver.Resolver()
+    answers = ip_address.resolve(search, 'A').rrset[0].to_text()
+    # del ip_address
+    # answer=ip_address.nameservers(search=search,qname='google.com').rrset[0].to_text()
+    # data = ipapi.location(ip=search, output='json')
+    # try:
+    #     ip_address = dns.resolver.resolve(search, 'A').rrset[0].to_text()
+    # except dns.resolver.NoAnswer:
+    #     ip_address = 'No answer'
+
+    # try:
+    #     ip_address = dns.resolver.resolve(search, 'A').rrset[0].to_text()
+    # except dns.resolver.NoAnswer:
+    #     ip_address = 'No answer'
+
+    # ip6=socket.getaddrinfo(search, None, socket.AF_INET6)[0][4][0]
+#untag
+    context = {"ip_address": answers}
+    # context={"data": data}
     return render(request, 'index.html', context)
+
--- IPFinder/settings.py
+++ IPFinder/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'IPFinder',
     'IPApp',
+    # 'records',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# COMMENT
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
